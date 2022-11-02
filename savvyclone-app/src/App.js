085be4a195e93ef6b334a642c833d774e4796106
Vishal M <vishal@divum.in>
2022-11-02 18:07:49 +0530

    Router config failed

--- savvyclone-app/src/App.js
+++ savvyclone-app/src/App.js
@@ -1,23 +1,24 @@
 import './App.scss';
 import React from 'react';
-import Header from './Components/Topbar';
 import Products from './Components/Products';
 import Customer from './Components/Customer';
 import Press from './Components/Press'
+import Header from './Components/Topbar';
 import Footer from './Components/Footer';
-
 
 
 
 function App() {
   return (
-  <>
+  <div >
+
     <Header />
     <Products />
     <Customer/>
     <Press />
     <Footer />
-  </>
+  
+  </div>
   );
 }
 
